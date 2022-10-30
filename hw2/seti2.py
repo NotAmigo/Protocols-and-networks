aa6d978b48ad0a1ad2a4393c7c6154deca711ffd
Ilya Kovalchuk <sckremyulock@gmail.com>
2022-10-30 23:23:47 +0500

    Beta hw2

--- hw2/seti2.py
+++ hw2/seti2.py
@@ -1,17 +1,43 @@
 import socket
-import binascii
+import sys
+
+import dnslib
+
 
 ADDRESS = ('www.yandex.ru')
-IP = '127.0.0.1'
+IP = '0.0.0.0'
 PORT = 65432
 ROOT = '192.203.230.10'
 DNSPORT = 53
 
+
+def recursive_dns_sniffer(info, address, addrdig):
+    s.sendto(info, address)
+    try:
+        data, addr = s.recvfrom(1024)
+    except ConnectionResetError:
+        print('ConnectionResetError')
+        sys.exit(1)
+    dns = dnslib.DNSRecord.parse(data)
+    if dns.header.a != 0:
+        s.sendto(data, addrdig)
+        return
+    if dns.header.rcode != 0:
+        s.sendto(data, addrdig)
+        return
+    for answer in dns.ar:
+        if answer.rtype == 1:
+            a = '.'.join(map(str, answer.rdata.data))
+            recursive_dns_sniffer(info, (a, DNSPORT), addrdig)
+        if answer.rtype == 41:
+            return
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((IP, PORT))
     datadig, addrdig = s.recvfrom(1024)
-    s.sendto(datadig, ('199.7.91.13', 53))
-    data, addr = s.recvfrom(1024)
-    s.sendto(data, addrdig)
-    print(data)
-    print(addr)
+    address = (ROOT, DNSPORT)
+    ans = recursive_dns_sniffer(datadig, address, addrdig)
+    if ans is None:
+        s.sendto(b'No answer', addrdig)
+        print('No answer')
