93046dd878ac6baf4e4570836d0b087f8c861dd0
Ilya Kovalchuk <sckremyulock@gmail.com>
2022-10-30 16:07:45 +0500

    Commit1

--- hw2/seti2.py
+++ hw2/seti2.py
@@ -1,19 +1,17 @@
 import socket
+import binascii
 
 ADDRESS = ('www.yandex.ru')
 IP = '127.0.0.1'
 PORT = 65432
+ROOT = '192.203.230.10'
+DNSPORT = 53
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.bind((IP, PORT))
-    s.listen(1)
-    conn, addr = s.accept()
-    print(conn)
+    datadig, addrdig = s.recvfrom(1024)
+    s.sendto(datadig, ('199.7.91.13', 53))
+    data, addr = s.recvfrom(1024)
+    s.sendto(data, addrdig)
+    print(data)
     print(addr)
-    with conn:
-        while True:
-            data = conn.recv(1024)
-            print(f'{data!r}')
-            if not data:
-                break
-            conn.sendall(data)
