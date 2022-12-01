ddd65cc351e8bb81aec7767ab1caadb272335e05
Ilya <sckremyulock@gmail.com>
2022-12-01 16:54:06 +0500

    Added IPv6

--- hw4/IPv4Packet.py
+++ hw4/IPv4Packet.py
@@ -1,4 +1,11 @@
+from scapy.all import IP
+
+
 class IPv4Packet:
-    def __init__(self, dest, ttl):
+    def __init__(self, dest, ttl, id):
         self.dest = dest
         self.ttl = ttl
+        self.id = id
+
+    def __call__(self):
+        return IP(dest=self.dest, ttl=self.ttl, id=self.id)
---
+++ hw4/IPv6Packet.py
@@ -0,0 +1,10 @@
+from scapy.all import IPv6
+
+
+class IPv4Packet:
+    def __init__(self, dest, ttl):
+        self.dest = dest
+        self.ttl = ttl
+
+    def __call__(self):
+        return IPv6(dest=self.dest, hlim=self.ttl)
