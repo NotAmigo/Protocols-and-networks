b501ffd46d0e2dcb58ab5af1320ba491da3b66b2
Ilya Kovalchuk <sckremyulock@gmail.com>
2022-12-04 14:59:17 +0500

    hw3

--- hw3/portscan.py
+++ hw3/portscan.py
@@ -16,43 +16,53 @@ def try_getservbyport(port):
         return "-"
 
 
-def try_get_type_packet(request, packet):
-    if packet is TCP:
-        return request[packet]
-    elif packet is UDP:
-        try:
-            return request[UDP]
-        except:
-            return None
-    else:
-        raise Exception("Unknown packet type")
-
-
-def get_flags(packet):
-    try:
-        return packet.flags
-    except:
-        return None
-
-
-def port_scanner(dst, ports, packet):
-    ans, unans = sr(IP(dst=dst) / packet(sport=1488, dport=ports), timeout=2, verbose=0)
+def tcp_scan(dst, ports):
+    ans, unans = sr(IP(dst=dst) / TCP(sport=1337, dport=ports, flags="S"), timeout=2, verbose=0)
     for answer in ans:
         request, response = answer
-        proto_request = request[packet]
-        proto_response = try_get_type_packet(response, packet)
-        flags = get_flags(proto_response)
-        if proto_response and flags and flags == 18: # TODO: rewrite on getlayer[TCP] and haslayer[TCP] f.e
-            port = proto_request.dport
-            protocol = proto_request.name
+        if response.haslayer(TCP):
+            port = request[TCP].dport
+            protocol = request[TCP].name
             application_protocol = try_getservbyport(port)
             timestamp = to_milliseconds(response.time - request.sent_time)
             print(f"{protocol} {port} {timestamp}ms {application_protocol}")
 
 
+def udp_scan(dst, ports):
+    ans, unans = sr(IP(dst=dst) / UDP(sport=1488, dport=ports) / DNS(rd=1, qd=DNSQR(qname=dst)), timeout=5, verbose=0)
+    for answer in ans:
+        request, response = answer
+        if response.haslayer(UDP):
+            port = request[UDP].dport
+            protocol = request[UDP].name
+            application_protocol = try_getservbyport(port)
+            timestamp = to_milliseconds(response.time - request.sent_time)
+            print(f"{protocol} {port} {timestamp}ms {application_protocol}")
+
+
+enum = {"tcp": tcp_scan, "udp": udp_scan}
+
+
 def get_args(ports):
-    for port in ports:
-        print(port)
+    answer = []
+    if not ports:
+        answer.append(("tcp", (1, 1000)))
+        return answer
+    for port in ports.split():
+        if "/" not in port:
+            answer.append((port, (1, 1000)))
+        else:
+            protocol, ports = port.split("/")
+            ports_range = []
+            ports = ports.split(",")
+            for inner_port in ports:
+                if "-" in inner_port:
+                    start, end = inner_port.split("-")
+                    ports_range += list(range(int(start), int(end) + 1))
+                else:
+                    ports_range.append(int(inner_port))
+            answer.append((protocol, ports_range))
+    return answer
 
 
 @click.command()
@@ -60,7 +70,16 @@ def get_args(ports):
 @click.option('--timeout', default=2, help='Timeout')
 @click.option('-g', '--guess', is_flag=True, help='Guess mode')
 @click.option('-j', '--num-threads', default=1, help='Number of threads')
-@click.argument('dst', nargs=1)
-@click.argument('ports', nargs=-1)
-def portscan(verbose, timeout, guess, num_threads, ports, dst):
-    print(dst)
+@click.argument('dst', nargs=1, required=True)
+@click.argument('ports', nargs=-1, required=False)
+def main(dst, ports):
+    args = get_args(ports)
+    for protocol, ports in args:
+        if protocol in enum:
+            enum[protocol](dst, ports)
+        else:
+            print(f"Unknown protocol {protocol}")
+
+
+if __name__ == '__main__':
+    main()
